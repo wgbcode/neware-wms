d94997455c10aa9c0610a497a13bf213233748ae
wuguangbin <wuguangbin@neware.com.cn>
2023-08-10 10:07:57 +0800

    1

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="c_h100p c_relative">
     <div class="c_absolute c_t10 c_l10">
-      <img src="@images/logo.png" alt="" />
+      <img src="@images/logo.png" />
     </div>
     <div class="c_absolute-center">form</div>
     <div class="c_absolute-xcenter c_b10">sign</div>
